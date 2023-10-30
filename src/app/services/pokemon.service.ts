ca9b3d7161569190d21ee70c5e3f243160b75b1a
Geanne1999 <>
2023-10-30 10:21:16 -0400

    feat(service): creating pokedex service

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  pokemons: Pokemon[] = [];
+  filteredPokemons: Pokemon[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.carregarPokemons();
+  }
+  async carregarPokemons() {
+    const request = await this.httpClient
+      .get<any>(' https://pokeapi.co/api/v2/pokemon?limit=1000')
+      .toPromise();
+
+    this.pokemons = request.results;
+    this.filteredPokemons = request.results;
+  }
+
+  async getPokemonById(id: number): Promise<Pokemon | undefined> {
+    try {
+      const response = await this.httpClient
+        .get<any>(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .toPromise();
+
+      return this.mapPokemonDetails(response);
+    } catch (error) {
+      console.error('Error fetching Pokemon details:', error);
+      return undefined;
+    }
+  }
+
+  filterPokemon(searchTerm: string): void {
+    if (searchTerm) {
+      this.filteredPokemons = this.pokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredPokemons = this.pokemons;
+    }
+  }
+
+  private mapPokemonDetails(data: any): Pokemon {
+    const pokemon: Pokemon = {
+      name: data.name,
+      number: data.id,
+      types: data.types.map((type: any) => type.type.name as Type),
+    };
+    return pokemon;
+  }
+}
+
+export interface Pokemon {
+  name: string;
+  types: Type[];
+  number: number;
+}
+
+enum Type {
+  Grass = 'Grass',
+  Poison = 'Poison',
+  Fire = 'Fire',
+}
